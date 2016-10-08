a0d573202ced84509a333e2d474f9998b761fd39
Jimeno0 <carlospj@icloud.com>
2016-10-08 11:37:55 +0200

    Cat clicker counting clicks!

--- cat-clicker/src/app/hello.js
+++ cat-clicker/src/app/hello.js
@@ -2,6 +2,43 @@
 
 angular.module('app')
   .controller('MyController', function(){
+    var self = this;
+    self.cats = [];
+    self.selected = null;
+       
     
-    
+
+    self.cats = [
+      { id: 1,
+        name: 'Coco',
+        src: '/images/coco.jpg',
+        count: 0},
+      { id: 2,
+        name: 'Sombra',
+        src: '/images/sombra.jpg',
+        count: 0},
+      { id: 3,
+        name: 'Frodo',
+        src: '/images/frodo.jpg',
+        count: 0},
+      { id: 4,
+        name: 'Scar',
+        src: '/images/scar.jpg',
+        count: 0},
+      { id: 5,
+        name: 'Felix',
+        src: '/images/felix.jpg',
+        count: 0}
+    ];
+
+    self.selected = self.cats[0];
+
+    self.select = function (cat) {
+      self.selected = cat;
+    };
+
+    self.catClick = function (cat) {
+      cat.count ++;
+    };
+
   });
